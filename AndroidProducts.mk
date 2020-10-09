8f8e6765765b42a748447ffc7d17fe0970370f60
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-10-09 11:03:34 +0000

    lmi initial commit
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosip_lmi.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosip_lmi-user \
+    aosip_lmi-userdebug \
+    aosip_lmi-eng
