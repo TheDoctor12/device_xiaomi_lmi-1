crdroidandroid/android_device_oneplus_sm8250-common

sm8250-common: power: Rebase on LA.UM.9.12.r1-09000-SMxx50.0

power/Android.mk
@@ -2,16 +2,20 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.hardware.power@1.2-service.oneplus_kona
-LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE := android.hardware.power-service.oneplus_kona
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_STEM := android.hardware.power@1.2-service
+LOCAL_MODULE_STEM := android.hardware.power-service
 
 LOCAL_CFLAGS += \
     -Wno-unused-parameter \
     -Wno-unused-variable
+
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
 
 LOCAL_C_INCLUDES := \
     external/libxml2/include \
@@ -24,23 +28,22 @@ LOCAL_HEADER_LIBRARIES += \
 LOCAL_SRC_FILES := \
     hint-data.c \
     list.c \
+    main.cpp \
     metadata-parser.c \
     power-common.c \
     Power.cpp \
     powerhintparser.c \
-    service.cpp \
     utils.c
 
 LOCAL_REQUIRED_MODULES := \
-    android.hardware.power@1.2-service.oneplus_kona.rc
+    android.hardware.power-service.oneplus_kona.rc
 
 LOCAL_SHARED_LIBRARIES := \
-    android.hardware.power@1.2 \
+    android.hardware.power-ndk_platform \
     libbase \
+    libbinder_ndk \
     libcutils \
     libdl \
-    libhidlbase \
-    libhidltransport \
     liblog \
     libutils \
     libxml2
@@ -49,13 +52,13 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.hardware.power@1.2-service.oneplus_kona.rc
+LOCAL_MODULE := android.hardware.power-service.oneplus_kona.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
-LOCAL_MODULE_STEM := android.hardware.power@1.2-service.rc
+LOCAL_MODULE_STEM := android.hardware.power-service.rc
 
-LOCAL_SRC_FILES := android.hardware.power@1.2-service.rc
+LOCAL_SRC_FILES := android.hardware.power-service.rc
 
 include $(BUILD_PREBUILT)

TheDoctor12/device_xiaomi_lmi-1

Change-Id: Ie71ce52b50af2446e93f44cd545ff8462612c8d7

power/Android.mk
@@ -2,19 +2,19 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.hardware.power@1.2-service.xiaomi_kona
-LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE := android.hardware.power-service.xiaomi_kona
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_STEM := android.hardware.power@1.2-service
+LOCAL_MODULE_STEM := android.hardware.power-service
 
 LOCAL_CFLAGS += \
     -Wno-unused-parameter \
     -Wno-unused-variable
 
-ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
-    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
 endif
 
 LOCAL_C_INCLUDES := \
@@ -28,23 +28,22 @@ LOCAL_HEADER_LIBRARIES += \
 LOCAL_SRC_FILES := \
     hint-data.c \
     list.c \
+    main.cpp \
     metadata-parser.c \
     power-common.c \
     Power.cpp \
     powerhintparser.c \
-    service.cpp \
     utils.c
 
 LOCAL_REQUIRED_MODULES := \
-    android.hardware.power@1.2-service.xiaomi_kona.rc
+    android.hardware.power-service.xiaomi_kona.rc
 
 LOCAL_SHARED_LIBRARIES := \
-    android.hardware.power@1.2 \
+    android.hardware.power-ndk_platform \
     libbase \
+    libbinder_ndk \
     libcutils \
     libdl \
-    libhidlbase \
-    libhidltransport \
     liblog \
     libutils \
     libxml2
@@ -53,13 +52,13 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.hardware.power@1.2-service.xiaomi_kona.rc
+LOCAL_MODULE := android.hardware.power-service.xiaomi_kona.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
-LOCAL_MODULE_STEM := android.hardware.power@1.2-service.rc
+LOCAL_MODULE_STEM := android.hardware.power-service.rc
 
-LOCAL_SRC_FILES := android.hardware.power@1.2-service.rc
+LOCAL_SRC_FILES := android.hardware.power-service.rc
 
 include $(BUILD_PREBUILT)