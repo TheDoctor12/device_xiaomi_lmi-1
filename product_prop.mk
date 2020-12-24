015667d2f7c7909a12262de9045c25f5df1a0e42
dianlujitao <dianlujitao@gmail.com>
2020-12-24 21:38:20 +0100

    lmi: Switch to QTI BT stack

--- product_prop.mk
+++ product_prop.mk
@@ -14,19 +14,10 @@ endif
 
 # Audio
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.bluetooth.a2dp_offload.supported=false \
-    persist.bluetooth.a2dp_offload.disabled=true \
-    vendor.audio.feature.a2dp_offload.enable=false \
-    persist.vendor.qcom.bluetooth.enable.splita2dp=false \
     ro.config.media_vol_steps=25 \
     ro.config.vc_call_vol_steps=7 \
     debug.stagefright.omx_default_rank.sw-audio=16 \
-    ro.control_privapp_permissions=log \
-    persist.vendor.bt.a2dp.aac_whitelist=false
-
-# Bluetooth
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.bluetooth.library_name=libbluetooth.so
+    ro.control_privapp_permissions=log
 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
