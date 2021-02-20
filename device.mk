d176a48bb20505afd152b04a3195334487ac1ad1
dianlujitao <dianlujitao@gmail.com>
2021-02-20 22:31:58 +0100

    lmi: Build Livedisplay

--- device.mk
+++ device.mk
@@ -134,6 +134,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl.xiaomi_kona
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
