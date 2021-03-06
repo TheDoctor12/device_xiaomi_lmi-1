e8fa30cdec7cba5bca61c31770e6d0f9e6caa9d7
RealSchnedi <schnedi3@gmail.com>
2021-03-06 20:20:54 +0100

    lmi: Build System helper

--- device.mk
+++ device.mk
@@ -215,6 +215,10 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/xiaomi \
     vendor/nxp/opensource/sn100x
 
+# System Helper
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.systemhelper@1.0
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
